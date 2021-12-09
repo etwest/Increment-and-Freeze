d911dbc051c44d380039f6e0beed4d126824f7be
daniel delayo <dandelayo@gmail.com>
2021-12-09 17:28:53 +0000

    added comments to OSTree

--- include/OSTree.h
+++ include/OSTree.h
@@ -4,28 +4,74 @@
 #include <utility>
 #include <vector>
 
+// Order Statistic Tree
+// Must be managed by OSTreeHead
 class OSTree {
     private:
+        // The count of nodes in the tree, including yourself
+        // The weight of a nullptr is 0.
         size_t weight;
         OSTree* left = nullptr;
         OSTree* right = nullptr;
 
+        // Timestamp
         uint64_t ts;
+        // Value-- such as address
         uint64_t value;
 
+        /* Returns a sorted array of <timestamp, value> pairs
+         * Used to rebalance the tree
+        */
         std::vector<std::pair<uint64_t, uint64_t>> to_array();
 
+        /* Rebalances the tree, including rerooting
+        * Hint: Should only be called when bad_balance() returns true
+        *       To retain an amortized rebalance cost of O(1)
+        */
         void rebalance();
+
+        /* Returns the left (or right) child pointer passed in, or a new one if null.
+         * child:   (possibly null) child pointer
+         * array:   sorted array
+         * first:   first element we must place in our subtree
+         * last:    last element we must place in our subtree, inclusive
+         * Recursively builds a balanced tree.
+         * Hint:    If a node exists, we adjust its values to desired. 
+         *          If it does not exist, we create it. 
+         *          If it exists and should not, we delete its subtree
+        */
         OSTree *rebalance_helper(OSTree *child, std::vector<std::pair<uint64_t, uint64_t>>& array, 
                 size_t first, size_t last);
+        //Returns whether or not the subtree needs to be rebalanced
+        //true ->   must be rebalanced
+        //false -> should not be rebalanced
         bool bad_balance();
     public:
+        /* Inserts a new entry into the tree with key/timestamp ts and value val
+         * ts:  The key to sort on.
+         * val: The value to store
+         * May rebalance the tree
+        */
         void insert(uint64_t ts, uint64_t val);
+        /* Removes the element with rank predecessors
+         * May rebalance the tree
+        */
         void remove(size_t rank);
+
         //uint64_t rank(size_t rank); //what element is rank X
+        
+        /* Returns the rank and value of element with key ts
+         * ts:      The key to search for
+         * Note:    ts must exist in the tree
+        */
         std::pair<size_t, uint64_t> find(uint64_t ts); //what rank is X at
 
-        OSTree(uint64_t, uint64_t);
+        /* Constructs a new Order Statistic tree node
+         * ts:  key
+         * value: value
+        */
+        OSTree(uint64_t ts, uint64_t value);
+        // Destructs a node and its subtree recursively
         ~OSTree();
 
         size_t get_weight() { return weight; };
@@ -33,11 +79,14 @@ class OSTree {
         OSTree* get_left()  { return left; };
         uint64_t get_val()  { return value; };
 
+
+        // Returns the element of rank 0
         OSTree* get_leftmost() {
             if (left == nullptr)
                 return this;
             return left->get_leftmost();
         };
+        // Returns the element of max rank
         OSTree* get_rightmost() {
             if (right == nullptr)
                 return this;
@@ -45,34 +94,64 @@ class OSTree {
         };
 };
 
+/* Manages an OSTree
+ * Handles edge cases 
+*/
 class OSTreeHead {
     public:
+        //Pointer to the root of the managed OSTree
         OSTree* head = nullptr;
+
+        //Destructs the head and entire managed tree, recursively
         ~OSTreeHead() {if (head != nullptr) delete head;};
+        
+        
+        /* Inserts a new node into the OSTree
+         * ts: key to be inserted
+         * val: value to be inserted
+        */
         void insert(uint64_t ts, uint64_t val) {
             if (head == nullptr)
                 head = new OSTree(ts, val);
             else
                 head->insert(ts, val);
         };
+
+        /* removes a node from the OSTree
+         * rank: rank of node to be removed
+        */
         void remove(size_t rank) {
             assert(head != nullptr);
             assert(rank < head->get_weight());
             if (head->get_weight()== 1)
+            {
                 delete head;
+                head = nullptr;
+            }
             else
                 head->remove(rank);
         };
+
+        /* returns a <rank, value> pair given a key
+         * ts:   key to search on
+         * Note: ts must exist in the tree
+        */
         std::pair<size_t, uint64_t> find(uint64_t ts) {
             assert(head != nullptr);
             return head->find(ts);
         };
 
-        //TODO: This can be done during remove instead
+        /* TODO: This can be done during remove instead
+         * returns the value of the element of highest rank
+         * Note: Tree must be contain at least one element
+        */
         uint64_t get_last() {
             assert(head != nullptr);
             return head->get_rightmost()->get_val();
         }
+        /* Returns the number of elements in the tree
+         * The weight of a nullptr is 0
+        */
         size_t get_weight() {
             return head == nullptr? 0 : head->get_weight();
         }
--- src/OSTree.cpp
+++ src/OSTree.cpp
@@ -1,10 +1,12 @@
 #include "OSTree.h"
 
+// All new trees have weight 1, as there are no children
 OSTree::OSTree(uint64_t ts, uint64_t value)
     : ts(ts), value(value) {
     weight = 1;
 };
 
+// Recursively delete the subtrees
 OSTree::~OSTree() {
     if (left != nullptr)
         delete left;
@@ -14,10 +16,15 @@ OSTree::~OSTree() {
 
 void OSTree::insert(uint64_t newts, uint64_t newval) {
     assert(newts != ts);
-
+    
+    // Ensure the tree is balanced on the way down
+    // The tree may get out of balance after inserting,
+    // but only by 1 element
     if (bad_balance())
         rebalance();
 
+    // larger keys are inserted to the left
+    // the element with oldest/smallest timestamp is the rightmost
     if (newts > ts) {
         if (left == nullptr)
             left = new OSTree(newts, newval);
@@ -32,6 +39,7 @@ void OSTree::insert(uint64_t newts, uint64_t newval) {
     }
     weight++;
 
+    // Ensure that the weights are still maintained properly
     if (left == nullptr)
         assert(weight == 1 + right->weight);
     else if (right == nullptr)
@@ -40,9 +48,13 @@ void OSTree::insert(uint64_t newts, uint64_t newval) {
         assert(weight == 1 + left->weight + right->weight);
 }
 
+// Removes a child with rank predecessors
 void OSTree::remove(size_t rank) {
+    //Ensure the element exists and there is a child to remove
     assert(rank < weight);	
     assert(weight > 1);
+
+    // Ensure that the weights are still maintained properly
     if (left == nullptr)
         assert(weight == 1 + right->weight);
     else if (right == nullptr)
@@ -50,6 +62,9 @@ void OSTree::remove(size_t rank) {
     else
         assert(weight == 1 + left->weight + right->weight);
 
+    // Rebalance the tree on the way down.
+    // This may cause us to rebalance more than checking after,
+    // But does not violate the O(1) amortized cost
     if (bad_balance())
         rebalance();
 
@@ -58,12 +73,18 @@ void OSTree::remove(size_t rank) {
     size_t lweight = left == nullptr? 0 : left->weight;
     size_t rweight = right == nullptr? 0 : right->weight;
 
+    // Delete ourself
+    // Instead of invoking the memory manager on ourselves, we impersonate 
+    // the next or previous element and delete that instead
+    // Such element must exist, as weight > 1
     if (lweight == rank) {
+        //For the sake of balance, delete from the heaviest side.
         OSTree* deleteMe = lweight > rweight ? left->get_rightmost(): right->get_leftmost();	
         // We can't delete outselves, but we can pretend the be the next element!
         ts = deleteMe->ts;
         value = deleteMe->value;
         if (lweight > rweight) {
+            // Recursive base case
             if (lweight == 1) {
                 delete left;
                 left = nullptr;
@@ -72,6 +93,7 @@ void OSTree::remove(size_t rank) {
                 left->remove(lweight-1);	
         }
         else {
+            //Recursive base case
             if (rweight == 1) {
                 delete right;
                 right = nullptr;
@@ -81,21 +103,27 @@ void OSTree::remove(size_t rank) {
         }
     }
     else if (rank < lweight) {
+        //delete from the left subtree
+        
         if (lweight == 1) {
             delete left;
             left = nullptr;
         }
         else if (lweight != 0)
+            // We skip over 0 elements, so rank remains the same
             left->remove(rank);
         else
             assert(false);
     }	
     else {
+        // delete from the right subtree
+        
         if (rweight == 1) {
             delete right;
             right = nullptr;
         }
         else if (rweight != 0)
+            // We skip over leftweight elements and ourselves
             right->remove(rank - left->weight - 1);
         else
             assert(false);
@@ -106,15 +134,16 @@ void OSTree::remove(size_t rank) {
 std::pair<size_t, uint64_t> OSTree::find(uint64_t searchts) {
     size_t lweight = left == nullptr? 0 : left->weight;
 
-    if (searchts == ts)
+    if (searchts == ts) //return ourselves
         return {lweight, value};
-    else if (searchts > ts) {
+    else if (searchts > ts) { //check left
         assert(left != nullptr);
         return left->find(searchts);
     }	
-    else {
+    else { //check right
         assert(right != nullptr);
         std::pair<size_t, uint64_t> answer = right->find(searchts);
+        // We skip over leftweight elements plus ourselves
         answer.first += 1 +lweight;
         return answer;
     }
@@ -123,25 +152,31 @@ std::pair<size_t, uint64_t> OSTree::find(uint64_t searchts) {
 std::vector<std::pair<uint64_t, uint64_t>> OSTree::to_array() {
     std::vector<std::pair<uint64_t, uint64_t>> larr;
     std::vector<std::pair<uint64_t, uint64_t>> rarr;
+    //build the left sorted array
     if (left != nullptr) {
         larr = left->to_array();
     }
 
+    //build the right sorted array
     if (right != nullptr) {
         rarr = right->to_array();
     }
+    // add ourselves after left
     larr.push_back({ts, value});
-    // TODO: what happens if rarr is empty
+    // add the right sorted array after ourselves
     larr.insert(larr.end(), rarr.begin(), rarr.end());
     return larr;
 }
 
 void OSTree::rebalance() {
+    // Turn our subtree into a sorted array
     std::vector<std::pair<uint64_t, uint64_t>> arr_rep = to_array();
+    // Impersonate the median element
     size_t mid = arr_rep.size() / 2;
     ts    = arr_rep[mid].first;
     value = arr_rep[mid].second;
 
+    // rebuild the left and right subtrees
     left  = rebalance_helper(left,  arr_rep, 0, mid - 1);
     right = rebalance_helper(right, arr_rep, mid + 1, arr_rep.size() - 1);
     // total weight shouldn't change
@@ -155,13 +190,16 @@ OSTree *OSTree::rebalance_helper(OSTree *child, std::vector<std::pair<uint64_t, 
     if (child == nullptr)
         child = new OSTree(0,0);
 
+    //Find the middle element without integer overflow
     size_t mid   = ((last - first) / 2) + first;
+
+    // impersonate the middle element
     child->ts    = arr_rep[mid].first;
     child->value = arr_rep[mid].second;
     child->weight = 1 + last-first;
 
     if (first == last) { // basecase
-        // Just us-- no children
+        // Just us-- delete the children
         if (child->left != nullptr) {
             delete child->left;
             child->left = nullptr;
@@ -171,7 +209,7 @@ OSTree *OSTree::rebalance_helper(OSTree *child, std::vector<std::pair<uint64_t, 
             child->right = nullptr;
         }
     }
-    else if (first == last-1) {
+    else if (first == last-1) { // second base case: two elements
         if (first == mid) { // Child to the right
             if (child->left != nullptr) {
                 delete child->left;
@@ -188,16 +226,19 @@ OSTree *OSTree::rebalance_helper(OSTree *child, std::vector<std::pair<uint64_t, 
         }
 
     }
-    else {
+    else { //recursively rebuild both left and right
         child->left  = child->rebalance_helper(child->left, arr_rep, first, mid - 1);
         child->right = child->rebalance_helper(child->right, arr_rep, mid + 1, last);
     }
+    //ensure weights were properly set during rebulding
     size_t lweight = child->left == nullptr? 0 : child->left->weight;
     size_t rweight = child->right == nullptr? 0 : child->right->weight;
     assert(child->weight == lweight + rweight + 1);
     return child;
 }
 
+// Returns true if either subtree + the parent has twice the weight of the other.
+// hint: The parent must be included to prevent infinite rebalance in the 2 node case
 bool OSTree::bad_balance() {
     size_t lweight = left == nullptr? 1 : left->weight + 1;
     size_t rweight = right == nullptr? 1 : right->weight + 1;
