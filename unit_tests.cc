ebb04bbf49707bbb0079b330013a04583f1d761f
Bradley Kuszmaul <kuszmaul@gmail.com>
2022-10-09 14:20:26 -0400

    Use EXPECT rather than ASSERT in tests

--- unit_tests.cc
+++ unit_tests.cc
@@ -47,11 +47,13 @@ TEST_P(CacheSimUnitTests, SimpleTest) {
 
   // get success function
   SuccessVector svec = sim->get_success_function();
-  ASSERT_GE(svec.size(), 3); // unique ids + 1
-  ASSERT_EQ(svec[1], 1);
-  ASSERT_EQ(svec[2], 2);
-  for (size_t i = 3; i < svec.size(); i++) {
-    ASSERT_EQ(svec[i], 2); // assert rest only get 2 hits
+  EXPECT_GE(svec.size(), 3); // unique ids + 1
+  if (svec.size() >= 3) {
+    EXPECT_EQ(svec[1], 1);
+    EXPECT_EQ(svec[2], 2);
+    for (size_t i = 3; i < svec.size(); i++) {
+      EXPECT_EQ(svec[i], 2); // assert rest only get 2 hits
+    }
   }
 }
 
@@ -80,15 +82,17 @@ TEST_P(CacheSimUnitTests, ValidateSuccess) {
 
   // get success function
   SuccessVector svec = sim->get_success_function();
-  ASSERT_GE(svec.size(), 7); // unique ids + 1
-  ASSERT_EQ(svec[1], 0);
-  ASSERT_EQ(svec[2], 1 * repeats);
-  ASSERT_EQ(svec[3], 2 * repeats);
-  ASSERT_EQ(svec[4], 6 * repeats);
-  ASSERT_EQ(svec[5], 7 * repeats - 1);
-  ASSERT_EQ(svec[6], 12 * repeats - 6);
-  for (size_t i = 7; i < svec.size(); i++) {
-    ASSERT_EQ(svec[i], 12 * repeats - 6); // assert rest are same
+  EXPECT_GE(svec.size(), 7); // unique ids + 1
+  if (svec.size() >= 7) {
+    EXPECT_EQ(svec[1], 0);
+    EXPECT_EQ(svec[2], 1 * repeats);
+    EXPECT_EQ(svec[3], 2 * repeats);
+    EXPECT_EQ(svec[4], 6 * repeats);
+    EXPECT_EQ(svec[5], 7 * repeats - 1);
+    EXPECT_EQ(svec[6], 12 * repeats - 6);
+    for (size_t i = 7; i < svec.size(); i++) {
+      EXPECT_EQ(svec[i], 12 * repeats - 6); // assert rest are same
+    }
   }
 }
 
@@ -120,15 +124,17 @@ TEST_P(CacheSimUnitTests, MultipleSuccessCalls) {
 
     // get success function
     SuccessVector svec = sim->get_success_function();
-    ASSERT_GE(svec.size(), 7); // unique ids + 1
-    ASSERT_EQ(svec[1], 0);
-    ASSERT_EQ(svec[2], 1 * (l+1) * rep_per_loop);
-    ASSERT_EQ(svec[3], 2 * (l+1) * rep_per_loop);
-    ASSERT_EQ(svec[4], 6 * (l+1) * rep_per_loop);
-    ASSERT_EQ(svec[5], 7 * (l+1) * rep_per_loop - 1);
-    ASSERT_EQ(svec[6], 12 * (l+1) * rep_per_loop - 6);
-    for (size_t i = 7; i < svec.size(); i++) {
-      ASSERT_EQ(svec[i], 12 * (l+1) * rep_per_loop - 6); // assert rest are same
+    EXPECT_GE(svec.size(), 7); // unique ids + 1
+    if (svec.size() >= 7) {
+      EXPECT_EQ(svec[1], 0);
+      EXPECT_EQ(svec[2], 1 * (l+1) * rep_per_loop);
+      EXPECT_EQ(svec[3], 2 * (l+1) * rep_per_loop);
+      EXPECT_EQ(svec[4], 6 * (l+1) * rep_per_loop);
+      EXPECT_EQ(svec[5], 7 * (l+1) * rep_per_loop - 1);
+      EXPECT_EQ(svec[6], 12 * (l+1) * rep_per_loop - 6);
+      for (size_t i = 7; i < svec.size(); i++) {
+        EXPECT_EQ(svec[i], 12 * (l+1) * rep_per_loop - 6); // assert rest are same
+      }
     }
   }
 }
