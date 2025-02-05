225158905c80effeb3f6f27fb75c46de008735f9
Daniel DeLayo <dandelayo@gmail.com>
2025-02-05 13:46:34 -0500

    handle empty success vector on dump

--- cache_sim.h
+++ cache_sim.h
@@ -91,7 +91,7 @@ class CacheSim {
     }
 
     // Finally dump the number of forced misses
-    size_t misses = total_requests - succ[succ.size() - 1];
+    size_t misses = total_requests - (succ.size() ? succ[succ.size() - 1] : 0);
     os << std::setw(16) <<"Misses" << std::setw(16) << misses 
        << std::setw(16) << percent(misses, total_requests) << "%" << std::endl;
   }
