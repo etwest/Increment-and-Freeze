b34d5e867aa257fccaaee576d7cfbe64ac5fff0e
Daniel DeLayo <dandelayo@gmail.com>
2025-04-02 11:40:55 -0400

    hash function fixes

--- bounded_iaf.cc
+++ bounded_iaf.cc
@@ -38,12 +38,13 @@ void BoundedIAF::memory_access(req_count_t addr) {
   if (sample_rate > 0) {
     // compute the hash of the input
     //uint64_t hash = XXH3_64bits_withSeed(&addr, sizeof(addr), sample_seed);
-    uint64_t hash = ((addr + sample_seed) * 226644670681749913l) >> 32;
+    uint64_t hash = ((__int128_t)(addr) * 13208052345836349601ull) >> 64;
 
     // ignore all requests whose hash value is incorrect
    // likely_if ((hash & sample_rate) != 0) return;
     //FIXME: This probably optimizes worse, but it's useful for testing
     likely_if ((hash % (sample_rate + 1)) != sample_partition) return;
+    
   }
   
   // small optimization, first check that the request is not a repeated request
