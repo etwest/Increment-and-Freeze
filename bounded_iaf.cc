980ba67a9450862e7fb3bed421ec0e2e4c855aa5
Daniel DeLayo <dandelayo@gmail.com>
2025-04-09 13:05:55 -0400

    cityhash

--- bounded_iaf.cc
+++ bounded_iaf.cc
@@ -29,7 +29,8 @@
 
 #include "increment_and_freeze.h"
 
-#include "xxh3.h"
+//#include "xxh3.h"
+#include "city.h"
 
 void BoundedIAF::memory_access(req_count_t addr) {
   ++access_number;
@@ -38,7 +39,8 @@ void BoundedIAF::memory_access(req_count_t addr) {
   if (sample_rate > 0) {
     // compute the hash of the input
     //uint64_t hash = XXH3_64bits_withSeed(&addr, sizeof(addr), sample_seed);
-    uint64_t hash = ((__int128_t)(addr) * 13208052345836349601ull) >> 64;
+   // uint64_t hash = ((__int128_t)(addr) * 13208052345836349601ull) >> 64;
+    uint64_t hash = CityHash64((char*)&addr, sizeof(addr));
 
     // ignore all requests whose hash value is incorrect
    // likely_if ((hash & sample_rate) != 0) return;
