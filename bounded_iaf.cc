9fdd9f01c290a948987c66229315c6c46ddfd27c
Daniel DeLayo <ddelayo@cs.stonybrook.edu>
2025-04-15 15:47:01 -0400

    mult + fold

--- bounded_iaf.cc
+++ bounded_iaf.cc
@@ -44,10 +44,10 @@ void BoundedIAF::memory_access(req_count_t addr) {
     //uint64_t hash = ((__int128_t)(addr) * 13208052345836349601ull) >> 64;
 
     // Current attempt
-    uint64_t hash = CityHash64((char*)&addr, sizeof(addr));
+    uint64_t hash = ((__int128_t)(addr) * 13208052345836349601ull) >> 64;
     // Fold the output into 16 bits 
     for (int i = 3; i > 0; --i)
-      hash ^= (hash >> (16*i));
+      hash ^= (uint16_t)(hash >> (16*i));
   
 
     // ignore all requests whose hash value is incorrect
