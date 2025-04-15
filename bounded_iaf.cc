477276d9b531f0172ff507ea5bfe77b291cde2b1
Daniel DeLayo <dandelayo@gmail.com>
2025-04-15 12:31:34 -0400

    ;

--- bounded_iaf.cc
+++ bounded_iaf.cc
@@ -47,7 +47,7 @@ void BoundedIAF::memory_access(req_count_t addr) {
     uint64_t hash = CityHash64((char*)&addr, sizeof(addr));
     // Fold the output into 16 bits 
     for (int i = 3; i > 0; --i)
-      hash ^= (hash >> (16*i))
+      hash ^= (hash >> (16*i));
   
 
     // ignore all requests whose hash value is incorrect
