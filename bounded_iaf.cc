037e5ae884f6906a019aa8ec230ae276238b74fb
Daniel DeLayo <dandelayo@gmail.com>
2026-01-27 12:29:58 -0500

    moving count to should_sample

--- bounded_iaf.cc
+++ bounded_iaf.cc
@@ -35,6 +35,7 @@ constexpr uint64_t rand_64b = 9316249495263525862ull;
   
 // Returns true if this address will be included
 bool BoundedIAF::should_sample(req_count_t addr) {
+  ++access_number;
   if (sample_mask > 0) {
     // Universal Hashing from wikipedia
     __int128_t hash_big = (__int128_t)prime_64b * addr;// + rand_64b;
@@ -43,7 +44,7 @@ bool BoundedIAF::should_sample(req_count_t addr) {
 
     // ignore all requests whose hash value is incorrect
     // OLD VERSION
-    // likely_if ((hash & sample_mask) != sample_partition) return false;
+    //likely_if ((hash & sample_mask) != sample_partition) return false;
     return (hash & sample_mask) == sample_partition;
   }
   return true;
@@ -51,7 +52,6 @@ bool BoundedIAF::should_sample(req_count_t addr) {
 
 void BoundedIAF::memory_access(req_count_t addr) {
   auto &requests = chunk_input.requests;
-  ++access_number;
 
   if (!should_sample(addr)) return;
 
--- increment_and_freeze.cc
+++ increment_and_freeze.cc
@@ -30,7 +30,7 @@ constexpr uint64_t prime_32b = 2147483647;
 
 
 bool IncrementAndFreeze::should_sample(req_count_t addr) {
-
+  ++access_number;
   if (sample_mask > 0) {
     // Universal Hashing from wikipedia
     __int128_t hash_big = (__int128_t)prime_64b * addr;// + rand_64b;
@@ -47,9 +47,8 @@ bool IncrementAndFreeze::should_sample(req_count_t addr) {
 
 void IncrementAndFreeze::memory_access(req_count_t addr) {
 
-  ++sample_access_number;
   if (!should_sample(addr)) return;
-  ++access_number;
+  ++sample_access_number;
 
   // catch case where new request is the same as the last
   // in this case we can just drop this request and increment
