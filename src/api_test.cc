0935eb9486823487de2dd2c4a33ecbe7f55f3d62
Daniel DeLayo <dandelayo@gmail.com>
2026-03-19 10:05:27 -0400

    simple api test

---
+++ src/api_test.cc
@@ -0,0 +1,15 @@
+#include "iaf_api.h"
+#include <iostream>
+
+int main()
+{
+    Iaf h = nullptr;
+    h = Iaf_create(0, 1000);
+    Iaf_write(h, (void*)1);
+    Iaf_write(h, (void*)-1);
+    Iaf_print(h);
+    Iaf_destroy(h);
+
+    std::cout << "h: " << h << std::endl;
+
+}
