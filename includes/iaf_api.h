3b603069c075fd9929ed1e2b2168c31d3aad132d
Daniel DeLayo <dandelayo@gmail.com>
2026-07-15 09:41:36 -0400

    graceful overflow handling

--- includes/iaf_api.h
+++ includes/iaf_api.h
@@ -13,7 +13,8 @@ typedef struct Iaf_t* Iaf;
 #define IAF_ID_UNINIT 0
 #define IAF_ID_NEED_REINIT 1
 #define IAF_ID_IGNORE 2
-#define IAF_ID_RESERVED_BOUNDARY 2 
+#define IAF_PAGE_OVERFLOW 3
+#define IAF_ID_RESERVED_BOUNDARY 3
 
 Iaf Iaf_create(int sampling_log2, size_t max_cache_size);
 bool Iaf_write(Iaf h, void* addr);
